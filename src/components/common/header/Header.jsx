9db4ad10e6f47d6f6633a0ecab067eacd46c0726
Maunducyrus <maunducyrus123@gmail.com>
2025-01-07 20:13:22 +0300

    Header.jsx

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container flex">
+                <div className="logo">
+
+                </div>
+                    <img src="./images/logo.png" alt="" />
+            </div>
+        </header>
+    )
+}
