cb6f0627d9fe67123272e6e524c269c52e09c0fc
Maunducyrus <maunducyrus123@gmail.com>
2025-01-08 21:06:51 +0300

    ...

--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../common/header/Header"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const pages = () => {
     return (
