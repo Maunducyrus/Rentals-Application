06976c622cac2dd1465cfb05234053b46ed51722
Maunducyrus <maunducyrus123@gmail.com>
2025-01-10 00:09:04 +0300

    creating the nav bar

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -4,13 +4,14 @@ import { nav } from "../../data/Data"
 
 const Header = () => {
     return (
+        <>
         <header>
             <div className="container flex">
                 <div className="logo">
 
                 </div>
                     <img src="./images/logo.png" alt="" />
-            </div>
+            
             <div className="nav">
                 <ul className="flex">
                     {nav.map((list, index) => (
@@ -24,8 +25,13 @@ const Header = () => {
                 <h4>
                 <span>2</span> My List
                 </h4>
-                <button className="btn1"></button>
+                <button className="btn1">
+                    <i className="fa fa-sign-out"></i>
+                </button>
+            </div>
             </div>
         </header>
+
+        </>
     )
 }
