4cf133ab9e07a0bda4199634eaec96dbcf60d259
Maunducyrus <maunducyrus123@gmail.com>
2025-01-24 14:12:49 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-// import "./App.CSS"
-// import pages from "./components/pages/pages"
-
-// function App() {
-//     return (
-//         <>
-//             <pages />
-//         </>
-//     )
-// }
-
-import React from 'react';
+import "./App.css"
+import pages from "./components/pages/pages.jsx"
 
 function App() {
-    console.log('App is rendering...');
-    return <h1>Hello, World!</h1>;
+    return (
+        <>
+            <pages />
+        </>
+    )
 }
+
 
 export default App;
