646382a123ac674784e5653afe75931c81e6c539
Maunducyrus <maunducyrus123@gmail.com>
2025-01-24 13:57:48 +0300

    adding npm else removing vite scripts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.CSS"
-import pages from "./components/pages/pages"
+// import "./App.CSS"
+// import pages from "./components/pages/pages"
+
+// function App() {
+//     return (
+//         <>
+//             <pages />
+//         </>
+//     )
+// }
+
+import React from 'react';
 
 function App() {
-    return (
-        <>
-            <pages />
-        </>
-    )
+    console.log('App is rendering...');
+    return <h1>Hello, World!</h1>;
 }
+
+export default App;
