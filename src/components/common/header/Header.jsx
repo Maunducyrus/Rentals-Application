2d5a550692f731a8a53062433a5c41db90b78382
Maunducyrus <maunducyrus123@gmail.com>
2025-01-25 23:45:20 +0300

    adding onClick on the toggle button

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 import { nav } from "../../data/Data"
 
 const Header = () => {
-    Const [navlist, setnavlist] = useState()
+    Const [navlist, setnavlist] = useState(false)
     return (
         <>
         <header>
@@ -15,7 +15,7 @@ const Header = () => {
                 <img src="/images/logo.png" alt="Logo" />
             
             <div className='nav'>
-                <ul className='flex'>
+                <ul className={navlist ? "small" : "flex"}>
                     {nav.map((list, index) => (
                         <li key={index}>
                             <Link to={list.path}>{list.text}</Link>
@@ -33,11 +33,10 @@ const Header = () => {
             </div>
 
                 <div className="toggle">
-                    <button>
-                        <i className="fa fa-bars"></i>
-                    </button>
+                        <button onClick={() => setnavlist(!navlist)}>
+                        {navlist ? <i className="fa fa-times"></i> : <i 
+                        className="fa fa-bars"></i>}</button>
                 </div>
-                
             </div>
         </header>
 
