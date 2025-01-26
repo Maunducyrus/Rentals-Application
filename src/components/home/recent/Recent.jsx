09c2c2cb27e018c6d32bb15021314985adc0b8a1
Maunducyrus <maunducyrus123@gmail.com>
2025-01-26 17:04:25 +0300

    mapping title and subtitle for Heading in Recent.jsx

--- src/components/home/recent/Recent.jsx
+++ src/components/home/recent/Recent.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Heading from '../../common/Heading'
 
 const Recent = () => {
   return (
-  <>Recent</>
+    <>
+      <section className='recent padding'>
+        <div className="container">
+            <Heading title='Recent Property Listed' subtitle='Get the latest property listed now for your benefit - we value you'/>
+        </div>
+      </section>
+    </>
     )
 }
 
