f44c9440fbcb111855c089c2d6cbd66ac51b551a
Maunducyrus <maunducyrus123@gmail.com>
2025-01-28 20:59:06 +0300

    mapping in the items and index in the location folder

--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -6,8 +6,12 @@ const Location = () => {
     <>
     <div className="location padding">
         <div className="container">
-            <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-' />
+            <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+            <div className="content grid3 mstop">
+                {location.map((items, index) => {
+                    <div className="box"></div>
+                })}
+            </div>
         </div>
     </div>
     </>
