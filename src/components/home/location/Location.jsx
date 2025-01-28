3e082ad07e4d95434569fc780e62996f225fc0f0
Maunducyrus <maunducyrus123@gmail.com>
2025-01-28 21:01:54 +0300

    final commit of the day

--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -9,7 +9,12 @@ const Location = () => {
             <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
             <div className="content grid3 mstop">
                 {location.map((items, index) => {
-                    <div className="box"></div>
+                    return (
+                        <div className="box" key={index}>
+                            <img src={items.cover} alt="" />
+
+                        </div>
+                    )
                 })}
             </div>
         </div>
