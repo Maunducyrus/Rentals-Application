02ebab9dd97ee0531e102c335306f6e8e22b03f4
Maunducyrus <maunducyrus123@gmail.com>
2025-01-28 20:53:15 +0300

    title and subtitle for location

--- src/components/home/location/Location.jsx
+++ src/components/home/location/Location.jsx
@@ -6,7 +6,8 @@ const Location = () => {
     <>
     <div className="location padding">
         <div className="container">
-            
+            <Heading title='Explore By Location' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+' />
         </div>
     </div>
     </>
