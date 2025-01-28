eff2c3980755e1cfeafe393d3231c4e3b31bee89
Maunducyrus <maunducyrus123@gmail.com>
2025-01-28 20:03:20 +0300

    Awards.jsx content

--- src/components/home/awards/Awards.jsx
+++ src/components/home/awards/Awards.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './awards.css'
+import Heading from '../../common/Heading'
 
 export const Awards = () => {
   return (
     <>
-    
+    <div className="awards padding">
+      <div className="container">
+        <Heading />
+      </div>
+    </div>
     </>
   )
 }
