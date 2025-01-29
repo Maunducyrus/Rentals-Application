86a3942f8311cdd67b09d3a44b41d017f5bcdb82
Maunducyrus <maunducyrus123@gmail.com>
2025-01-29 19:48:30 +0300

    added key values

--- src/components/home/team/Team.jsx
+++ src/components/home/team/Team.jsx
@@ -6,7 +6,17 @@ export const Team = () => {
   <div className="team background">
     <div className="container">
     <Heading title='Our Featured Agents' subtitle='Meet Our Team members who are ready and have all what it takes to satisfy customer needs - all our team are qualified' />
-    
+
+    <div className="content mtop grid3">
+        {Team.map((val, index) => {
+            return (
+                <div className="box" key={index}>
+                    <button></button>
+                </div>
+            )
+            
+        })}
+    </div>
     </div>
   </div>
   </> 
