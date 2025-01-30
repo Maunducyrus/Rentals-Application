4b1c32b9a5387d2da39c99ad2eb9bfb7a0181669
Maunducyrus <maunducyrus123@gmail.com>
2025-01-30 16:43:55 +0300

    priceCard

--- src/components/home/price/Price.jsx
+++ src/components/home/price/Price.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { PriceCard } from './PriceCard'
 
 export const Price = () => {
   return (
     <>
     <section className="price padding">
         <div className="container">
-            <Heading />
+            <Heading title='Select Your Package' subtitle='Variety of favourable prices for you select what pleases you and within your budget' />
+            <PriceCard />
         </div>
     </section>
     </>
