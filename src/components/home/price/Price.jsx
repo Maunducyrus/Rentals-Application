d59bb33d5418c36c0271b6e93f7f6f4381d394e8
Maunducyrus <maunducyrus123@gmail.com>
2025-01-30 16:35:35 +0300

    added section area

--- src/components/home/price/Price.jsx
+++ src/components/home/price/Price.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Price = () => {
   return (
-    <div>Price</div>
+    <>
+    <section className="price padding">
+        
+    </section>
+    </>
   )
 }
