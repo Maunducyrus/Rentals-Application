9d15d7b7b91491bf9cc97385ac04d657cb1fa289
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 19:43:39 +0300

    imports

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import img from '../images/about.jpg'
+import Back from '../common/Back'
 import RecentCard from '../home/recent/RecentCard'
 
 export const Blog = () => {
   return (
     <>
     <section className='blog-out mb'>
-          {<Back name='' title='' cover={} />}
+          <Back name='' title='' cover={img} />
           <div className="container recent">
             <RecentCard />
           </div>
