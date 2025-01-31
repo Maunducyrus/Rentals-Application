33ac454997797f41c10608b146aaac2a9a274182
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 19:19:32 +0300

    export default Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Services = () => {
   return (
     <>
-    
+    <div className="service mb">
+        {<Back name='' title='' cover={} />}
+    </div>
     </>
   )
 }
+
+export default Services
