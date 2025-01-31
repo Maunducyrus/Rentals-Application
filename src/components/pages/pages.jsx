00f76b16f77500aa44c73742548874df5665d935
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 14:36:01 +0300

    included footer in pages

--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../common/header/Header.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Footer } from "../common/footer/Footer.jsx";
 
 const Pages = () => {
     return (
@@ -9,9 +10,8 @@ const Pages = () => {
             <Header />
             <Routes>
              <Route exact path="/" element={Home} />
-             {/* <Route path="/about" element={() => <div>About Page</div>} />
-             <Route path="/services" element={() => <div>Services Page</div>} /> */}
             </Routes>
+            <Footer />
         </Router>
         </>
     )
