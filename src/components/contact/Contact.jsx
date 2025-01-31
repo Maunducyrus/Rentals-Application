e5062e2af960cd5e46f33773c14f3f53dd074e01
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 20:16:10 +0300

    imports

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Back from '../common/Back'
+import './contact.css'
+import img from '../images/pricing.jpg'
 export const Contact = () => {
   return (
-    <></>
+    <>
+    <section className='contact mb'>
+        <Back name='Contact Us' title='Get Helps & Friendly Support' cover={img} />
+    </section>
+    </>
   )
 }
 
