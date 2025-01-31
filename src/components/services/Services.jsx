cd27ca5de80a226e66648941b5bdc361732e03c2
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 19:23:20 +0300

    import service img and imported Back.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Back from '../common/Back'
+import img from '../images/services.jpg'
 
 export const Services = () => {
   return (
     <>
     <div className="service mb">
-        {<Back name='' title='' cover={} />}
+        {/* {<Back name='' title='' cover={} />} */}
+        <Back name='' title='' cover={img} />
     </div>
     </>
   )
