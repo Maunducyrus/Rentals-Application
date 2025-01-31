34d8b00f9c9031529afcefcf2a6775631ba43450
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 19:33:30 +0300

    <FeaturedCard />

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Back from '../common/Back'
 import img from '../images/services.jpg'
+import FeaturedCard from '../home/featured/FeaturedCard'
 
 export const Services = () => {
   return (
     <>
     <div className="service mb">
         {/* {<Back name='' title='' cover={} />} */}
-        <Back name='' title='' cover={img} />
+        <Back name='Services' title='Services - All Services' cover={img} />
+        <div className="featured container">
+            <FeaturedCard />
+        </div>
     </div>
     </>
   )
