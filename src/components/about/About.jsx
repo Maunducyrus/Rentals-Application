ea839b4db333dc6fb9bb3f8f2047b0ad47189fe5
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 18:28:12 +0300

    imported back

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Back from '../common/Back'
 
 export const About = () => {
   return (
    <>
    <section className='about'>
-
+    <Back />
    </section>
    </>
   )
