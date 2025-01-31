c9bae51ab98da6c74de4e76da29f5b1fd4dca4f9
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 18:33:59 +0300

    routing about

--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../common/header/Header.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from "../common/footer/Footer.jsx";
+import About from "../about/About.jsx";
 
 const Pages = () => {
     return (
@@ -10,6 +11,7 @@ const Pages = () => {
             <Header />
             <Routes>
              <Route exact path="/" element={Home} />
+             <Route exact path="/about" element={About} />
             </Routes>
             <Footer />
         </Router>
