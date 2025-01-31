8d0580b526c47f499808e0c864b8ce8fa7a516e2
Maunducyrus <maunducyrus123@gmail.com>
2025-01-31 18:32:20 +0300

    imported about img

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import img from '../images/about.jpg'
 import Back from '../common/Back'
 
 export const About = () => {
   return (
    <>
    <section className='about'>
-    <Back name='About Us' title='About us - Who We Are ?' />
+    <Back name='About Us' title='About us - Who We Are ?' cover={img} />
    </section>
    </>
   )
